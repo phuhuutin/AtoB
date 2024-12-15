e077be7cdd1cba83a4fce0ae670768bf00a1db0a
phuhuutin <39242538+phuhuutin@users.noreply.github.com>
2024-12-15 14:11:44 -0800

    updated more service

--- app/src/main/java/com/example/atob/networks/ShiftApiService.kt
+++ app/src/main/java/com/example/atob/networks/ShiftApiService.kt
@@ -1,27 +1,45 @@
 package com.example.atob.networks
 
-import com.example.atob.model.Shift
+import com.example.atob.model.FindShift
+import com.example.atob.model.UserShift
 import com.example.atob.model.ShiftDTO
+import com.example.atob.model.ClockInOutRecord
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface ShiftApiService {
-    @GET("/api/shift")
-    suspend fun getAllShifts(): Response<List<Shift>>
+    @GET("/api/shift/user")
+    suspend fun getAllShifts(): Response<List<UserShift>>
 
     @GET("/api/shift/{id}")
-    suspend fun getShiftById(@Path("id") id: Long): Response<Shift>
+    suspend fun getShiftById(@Path("id") id: Long): Response<UserShift>
+
+    @GET("/api/shift/getshifts/{employeeId}")
+    suspend fun getShiftsAfterNow(@Path("employeeId") employeeId: Long): Response<Set<FindShift>>
 
     @POST("/api/shift")
     suspend fun createShift(@Body shiftDTO: ShiftDTO): Response<String>
 
+    @PUT("/api/clock/{id}")
+    suspend fun udpateClock(@Path("id") id: Long, @Body clockInOutRecords: ClockInOutRecord): Response<String>
+
     @DELETE("/api/shift/{id}")
     suspend fun deleteShift(@Path("id") id: Long): Response<String>
 
+    @Headers("Content-Type: text/plain;charset=UTF-8")
+    @POST("/api/shift/clock/{shiftId}")
+    suspend fun addClockInOutRecord(@Path("shiftId") shiftId: Long): Response<String>
+
     @POST("/api/shift/{shiftId}/addEmployee")
     suspend fun addEmployeeToShift(@Path("shiftId") shiftId: Long): Response<String>
+
+
+    @POST("/api/shift/{id}/drop")
+    suspend fun dropShift(@Path("id") id: Long): Response<String>
 }
--- app/src/main/java/com/example/atob/networks/UserAPIService.kt
+++ app/src/main/java/com/example/atob/networks/UserAPIService.kt
@@ -1,31 +1,75 @@
 package com.example.atob.networks
 
+import com.example.atob.model.ChangePasswordRequest
+import com.example.atob.model.ClockInOutRecord
+import com.example.atob.model.ClockInOutRecordsUpdate
+import com.example.atob.model.InitialSetupDTO
 import com.example.atob.model.LoginRequest
+import com.example.atob.model.PayRateUpdateRequest
+import com.example.atob.model.Payroll
+import com.example.atob.model.Report
+import com.example.atob.model.ReportDTO
 import com.example.atob.model.SignUpRequest
 import com.example.atob.model.User
+import retrofit2.Response
 import retrofit2.http.*
 
 interface UserApiService  {
     @GET("api/user")
     suspend fun getAllUsers(): List<User>
 
-    @GET("api/user/{id}")
-    suspend fun getUserById(@Path("id") id: Long): User
+    @GET("api/user/id/{id}")
+    suspend fun getUserById(@Path("id") id: Long): Response<User>
 
-    @GET("api/user/{username}")
-    suspend fun getUserByUsername(@Path("username") username: String): User
+    @GET("api/user/username/{username}")
+    suspend fun getUserByUsername(@Path("username") username: String): Response<User>
+
+    @GET("api/user/payrolls")
+    suspend fun getPayrolls( ): Response<List<Payroll>>
 
     @POST("api/user")
     suspend fun createUser(@Body user: User): User
 
     @POST("api/user/login")
-    suspend fun login(@Body loginRequest: LoginRequest): User
+    suspend fun login(@Body loginRequest: LoginRequest): Response<User>
+
+
 
     @DELETE("api/user/{id}")
     suspend fun deleteUser(@Path("id") id: Long)
 
     @POST("api/user/signup")
-    suspend fun signUp(@Body signUpRequest: SignUpRequest): String
+    suspend fun signUp(@Body signUpRequest: SignUpRequest): Response<User>
+
+    @PUT("api/user/change-password")
+    suspend fun changePassword(@Body request: ChangePasswordRequest): Response<User>
+
+
+    @GET("api/reports")
+    suspend fun getAllReports(): Response<List<Report>>
+
+    @POST("api/reports")
+    suspend fun createReportForUser(
+        @Body report: ReportDTO
+    ): Response<Report>
+
+    @GET("api/reports/closed/{id}")
+    suspend fun closeReportById(@Path("id") id: Long): Response<Report>
+
+    @PUT("api/clock")
+    suspend fun updateClock(@Body clock: ClockInOutRecordsUpdate): Response<ClockInOutRecord>
+
+    @POST("api/user/setup")
+    suspend fun setUpEmployer(@Body data: InitialSetupDTO): Response<User>
+
+    @DELETE("api/employer/deleteRecord/{id}")
+    suspend fun deleteAttendanceRecord(@Path("id") id: Long): Response<String>
+
+    @PUT("api/user/payrate")
+    suspend fun updatePayRate(@Body payRateUpdateRequest: PayRateUpdateRequest): Response<String>
+
+
+
 }
 //interface UserService {
 //    suspend fun createUser(user: User): User
