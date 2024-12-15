ebfb0e0763f228bbf3349ddc9b578e4288f9b3b8
phuhuutin <39242538+phuhuutin@users.noreply.github.com>
2024-12-15 14:04:10 -0800

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,22 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
-
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
+//    id("com.google.secrets_gradle_plugin") version "0.6"
 }
 
 android {
     namespace = "com.example.atob"
-    compileSdk = 34
-
+    compileSdk = 35
+    buildFeatures {
+        buildConfig = true // Enable BuildConfig
+        compose = true
+    }
     defaultConfig {
         applicationId = "com.example.atob"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -20,15 +25,28 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+
+        val localProperties = Properties()
+        val localPropertiesFile = rootProject.file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localProperties.load(localPropertiesFile.inputStream())
+        }
+        buildConfigField("String", "LocalHost", "\"${localProperties.getProperty("LocalHost")}\"")
+        manifestPlaceholders["API_KEY"] = localProperties.getProperty("API_KEY")
+
+
+
     }
 
     buildTypes {
+
         release {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
         }
     }
     compileOptions {
@@ -38,9 +56,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
@@ -49,6 +65,7 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
 }
 
 dependencies {
@@ -68,14 +85,39 @@ dependencies {
     implementation(libs.androidx.constraintlayout.compose)
 
     // Retrofit
-    implementation(libs.retrofit2.kotlinx.serialization.converter)
     implementation(libs.retrofit)
+
     //Efficient image loading
     implementation(libs.coil.compose)
-    implementation(libs.kotlinx.serialization.json)
-    //navigation
+     //navigation
     implementation(libs.androidx.navigation.compose)
+    //Calendar
+    // The view calendar library for Android
+    implementation(libs.calendar.view)
 
+    // The compose calendar library for Android
+    implementation(libs.compose.v260)
+    // OkHttp dependency
+    implementation(libs.okhttp)
+   // implementation (libs.converter.scalars)
+    implementation(libs.converter.scalars)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+    implementation(libs.kotlinx.serialization.json.v151)
+
+    //   implementation(libs.retrofit2.kotlinx.serialization.converter.v080) // Kotlinx Serialization
+    implementation(libs.material)
+    implementation(libs.androidx.tools.core) // Kotlinx Serialization JSON library
+    //http logging intercept okhttp
+    implementation(libs.logging.interceptor)
+    implementation(libs.spring.security.crypto)
+    implementation(libs.commons.logging)
+
+    implementation(libs.androidx.room.runtime.android)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.play.services.location)
+    implementation (libs.play.services.maps)
+    implementation (libs.maps.compose)
+    implementation(libs.androidx.ui.test.android)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
